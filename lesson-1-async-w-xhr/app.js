81b4995df9cf02cbc17ee8e0655760c06f29de0a
Muhammad Gouda <77882114+iClay1998@users.noreply.github.com>
2021-07-25 20:42:37 +0200

    Add files via upload

---
+++ lesson-1-async-w-xhr/app.js
@@ -0,0 +1,60 @@
+(function () {
+    const form = document.querySelector('#search-form');
+    const searchField = document.querySelector('#search-keyword');
+    let searchedForText;
+    const responseContainer = document.querySelector('#response-container');
+
+    form.addEventListener('submit', function (e) {
+        e.preventDefault();
+        responseContainer.innerHTML = '';
+        searchedForText = searchField.value;
+        const imgRequest = new XMLHttpRequest();
+
+        imgRequest.onload = addImage;
+        imgRequest.onerror = function(err) {
+          requestError(err, 'image');
+        };
+        imgRequest.open('GET', `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`);
+        imgRequest.setRequestHeader('Authorization', 'Client-ID MZEljzgLtz4aVivG3FUfzvLdDbOodq_rqTpxy-KIZVE');
+        imgRequest.send();
+
+        function addImage(){
+          let htmlContent = '';
+          const data = JSON.parse(this.responseText);
+
+          if (data && data.results && data.results[0]) {
+            const firstImage = data.results[0];
+            htmlContent = `<figure>
+                <img src="${firstImage.urls.regular}" alt="${searchedForText}">
+                <figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>
+            </figure>`;
+          } else {
+            htmlContent = '<div class="error-no-image">No image available</dive>';
+          }
+
+          responseContainer.insertAdjacentHTML('afterbegin', htmlContent);
+        };
+
+        const articleRequest = new XMLHttpRequest();
+
+        articleRequest.onload = addArticle;
+        articleRequest.open('GET', `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=S0AAM2E3ghV5sAJ2sy7OBEGJXuHEYYbL`);
+        articleRequest.send();
+
+        function addArticle() {
+          let htmlContent = '';
+          const data = JSON.parse(this.responseText);
+
+          if (data.response && data.response.docs && data.response.docs.length > 1) {
+            htmlContent = "<ul>" + data.response.docs.map(article => `<li class="article">
+                    <h2><a href="${article.web_url}">${article.headline.main}</a></h2>
+                    <p>${article.snippet}</p>
+                </li>`).join('') + "</ul>"
+          } else {
+            htmlContent = `<div class="error-no-article">No articles available</dive>`
+          }
+
+          responseContainer.insertAdjacentHTML('beforeend', htmlContent);
+        };
+    });
+})();
